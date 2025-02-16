067ae22f3bc062f68fb89b63fd1b84c1425010ef
Ilya Shekordo <27phantom03@list.ru>
2025-02-17 00:12:40 +0300

    feat(app):some fix registration and activation page was added

--- src/store/userSlice.ts
+++ src/store/userSlice.ts
@@ -1,20 +1,61 @@
-import { createSlice } from "@reduxjs/toolkit";
+import {
+  asyncThunkCreator,
+  createAsyncThunk,
+  createSlice,
+} from "@reduxjs/toolkit";
+
+export const registrationUser = createAsyncThunk(
+  "user/registrationUser",
+  async (registrationData, { rejectWithValue }) => {
+    try {
+      const response = await fetch(
+        "https://studapi.teachmeskills.by/auth/users",
+        {
+          method: "POST",
+          body: JSON.stringify(registrationData),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (!response.ok) {
+        throw new Error("error");
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return rejectWithValue((error as Error).message);
+    }
+  }
+);
 
 const userSlice = createSlice({
   name: "user",
   initialState: {
-    email: "",
-    password: "",
+    user: null,
+    error: null as any,
+    loading: false,
   },
   reducers: {
-    setEmail: (state, action) => {
-      state.email = action.payload;
+    addUser: (state, action) => {
+      state.user = action.payload;
     },
-    setPassword: (state, action) => {
-      state.password = action.payload;
-    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(registrationUser.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.error = null;
+      })
+      .addCase(registrationUser.rejected, (state, action) => {
+        state.error = action.payload;
+        state.user = null;
+      })
+      .addCase(registrationUser.pending, (state) => {
+        state.loading = true;
+      });
   },
 });
 
 export default userSlice.reducer;
-export const { setEmail, setPassword } = userSlice.actions;
+export const { addUser } = userSlice.actions;
