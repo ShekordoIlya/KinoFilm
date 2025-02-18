124712fb5c935b408e4fccc6a3640e3a4d9d29bb
Ilya Shekordo <27phantom03@list.ru>
2025-02-18 20:55:17 +0300

    fix(app):some fixed react portal was done

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -3,8 +3,21 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { setSearchQuery, setOpenClose } from "../../store/filmSearchSlice";
 import Modal from "./Modal";
+import { useState } from "react";
 
 const Header = () => {
+  const { registrationData, setRegistrationData }: any = useState(null);
+  const formHandler = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(registrationData);
+  };
+  const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setRegistrationData((prev: any) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { searchQuery, openClose } = useSelector((state) => state.searchFilms);
@@ -41,7 +54,49 @@ const Header = () => {
           onClose={() => {
             dispatch(setOpenClose(!openClose));
           }}
-        ></Modal>
+        >
+          <form onSubmit={formHandler}>
+            <div className={style.emailWrapper}>
+              <p className={style.emailInfo}>
+                Введите адрес электронной почты:
+              </p>
+              <input
+                //   value={registrationData.email}
+                name="email"
+                onChange={inputHandler}
+                className={style.email}
+                type="email"
+              />
+            </div>
+            <div className={style.passWrapper}>
+              <p className={style.passInfo}>Введите пароль:</p>
+              <input
+                // value={registrationData.password}
+                onChange={inputHandler}
+                name="password"
+                className={style.pass}
+                type="password"
+              />
+            </div>
+            <div className={style.signInBtnWrapper}>
+              <button type="submit" className={style.signInBtn}>
+                Войти
+              </button>
+            </div>
+            <div className={style.registrationWrapper}>
+              <p>
+                Нет аккаунта? Нажмите{" "}
+                <a
+                  className={style.registrationLink}
+                  href={"/registrationPage"}
+                >
+                  сюда
+                </a>{" "}
+                чтобы создать аккаунт.
+              </p>
+            </div>
+          </form>
+        </Modal>
         <form className={style.wrapper} onSubmit={handlerSubmit}>
           <input
             type={"text"}
